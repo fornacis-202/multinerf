e335cbf841a07257218e2fc57825c74d84ce3d7b
fornacis-202 <77787902+fornacis-202@users.noreply.github.com>
2025-11-03 19:25:14 +0330

    Update train_blender.sh

--- scripts/train_blender.sh
+++ scripts/train_blender.sh
@@ -17,8 +17,8 @@ export CUDA_VISIBLE_DEVICES=0
 
 SCENE=ficus
 EXPERIMENT=blender
-DATA_DIR=/usr/local/google/home/barron/tmp/nerf_data/nerf_synthetic
-CHECKPOINT_DIR=/usr/local/google/home/barron/tmp/nerf_results/"$EXPERIMENT"/"$SCENE"
+DATA_DIR=/kaggle/input/nerf-dataset/nerf_synthetic/nerf_synthetic
+CHECKPOINT_DIR=/kaggle/working/res/"$EXPERIMENT"/"$SCENE"
 
 rm "$CHECKPOINT_DIR"/*
 python -m train \
